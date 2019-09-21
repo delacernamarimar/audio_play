fc2cb624a2b0b10d943f4b6082e6ff5a342f1cf0
marimard@codev.com <marimard@codev.com>
2019-09-21 13:39:45 +0800

    added 2 modes but no device for testing

--- app/src/main/java/com/example/audioplay/MainActivity.java
+++ app/src/main/java/com/example/audioplay/MainActivity.java
@@ -4,9 +4,11 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -16,14 +18,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
     TextToSpeech tts;
     EditText et_text_to_speech;
     Button btn_play_audio;
-    Switch sw_play_over_bluetooth;
+    Switch sw_play_over_bluetooth, sw_1, sw_2, sw_3;
     private AudioManager audioM;
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +41,16 @@ public class MainActivity extends AppCompatActivity {
         et_text_to_speech = findViewById(R.id.et_text_to_speech);
         btn_play_audio = findViewById(R.id.btn_play_audio);
         sw_play_over_bluetooth = findViewById(R.id.sw_play_over_bluetooth);
-        audioM = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        audioM.setMode(audioM.MODE_IN_COMMUNICATION);
-        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        sw_1 = findViewById(R.id.sw_1);
+        sw_2 = findViewById(R.id.sw_2);
+        sw_3 = findViewById(R.id.sw_3);
+        sw_play_over_bluetooth.setOnCheckedChangeListener(this);
+        sw_1.setOnCheckedChangeListener(this);
+        sw_2.setOnCheckedChangeListener(this);
+        sw_3.setOnCheckedChangeListener(this);
+        audioM = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+//        audioM.setMode(audioM.MODE_IN_COMMUNICATION);
+        tts = new TextToSpeech(getBaseContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status != TextToSpeech.ERROR) {
@@ -56,33 +68,39 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
                 }
-                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(getBaseContext(), toSpeak, Toast.LENGTH_SHORT).show();
             }
         });
 
-        sw_play_over_bluetooth.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                boolean on = isChecked;
-                if (on) {
-                    audioM.setBluetoothScoOn(true);
-                    audioM.startBluetoothSco();
-                    audioM.setSpeakerphoneOn(false);
-                } else {
-                    audioM.setBluetoothScoOn(false);
-                    audioM.setSpeakerphoneOn(true);
-                }
-            }
-        });
-    }
 
-    protected void onPause() {
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onPause();
+
+//        sw_play_over_bluetooth.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                boolean on = isChecked;
+//                Log.d("MainActivity", "onCheckedChanged: " + audioM.getMode());
+//                Log.d("MainActivity", "onCheckedChanged: " + Arrays.toString(audioM.getDevices(1)));
+//
+//
+//                if (on) {
+//                    audioM.setBluetoothScoOn(true);
+//                    audioM.startBluetoothSco();
+//                    audioM.setSpeakerphoneOn(false);
+//                    audioM.setMode(AudioManager.STREAM_SYSTEM);
+//                } else {
+//                    audioM.setBluetoothScoOn(false);
+//                    audioM.setSpeakerphoneOn(true);
+//                }
+//            }
+//        });
     }
+//
+//    protected void onPause() {
+//        if (tts != null) {
+//            tts.stop();
+//            tts.shutdown();
+//        }
+//        super.onPause();
+//    }
 
     protected void onDestroy() {
         // TODO Auto-generated method stub
@@ -90,4 +108,49 @@ public class MainActivity extends AppCompatActivity {
         audioM.setSpeakerphoneOn(true);
         super.onDestroy();
     }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        switch (compoundButton.getId()) {
+            case R.id.sw_1:
+                Log.d("MainActivity", "onCheckedChanged: " + audioM.getMode());
+                Log.d("MainActivity", "onCheckedChanged: " + Arrays.toString(audioM.getDevices(1)));
+
+
+                if (isChecked) {
+                    audioM.setBluetoothScoOn(true);
+                    audioM.startBluetoothSco();
+                    audioM.setSpeakerphoneOn(false);
+                } else {
+                    audioM.setBluetoothScoOn(false);
+                    audioM.setSpeakerphoneOn(true);
+                }
+
+                break;
+            case R.id.sw_2:
+                break;
+            case R.id.sw_3:
+                if (isChecked) {
+                    audioM.setMode(AudioManager.STREAM_MUSIC);
+                    audioM.setSpeakerphoneOn(true);
+                } else {
+                    audioM.setMode(AudioManager.STREAM_VOICE_CALL);
+                    audioM.setSpeakerphoneOn(false);
+                    audioM.setBluetoothScoOn(true);
+                    audioM.startBluetoothSco();
+                }
+                break;
+            case R.id.sw_play_over_bluetooth:
+                break;
+                default:
+        }
+    }
+
 }
+
+/* will test
+ generateAudioSessionId
+getActivePlaybackConfigurations
+getDevices
+
+ */
